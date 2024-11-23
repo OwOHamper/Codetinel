348be792d1749ace202028c588646a09ffb72f76
j4ku6-7 <j34543334@gmail.com>
2024-11-23 16:19:17 +0100

    added project.py

--- Backend/src/api/router/main.py
+++ Backend/src/api/router/main.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from src.api.routes import todos
+from src.api.routes import todos, project
 
 api_router = APIRouter()
-api_router.include_router(todos.router, prefix="/todos", tags=["todos"])
+
+api_router.include_router(project.router, prefix="/project", tags=["project"])
