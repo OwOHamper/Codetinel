0d96f37c5c79afe7974f90a5902d2d17487ac5e8
tomas-sedlak <t.sedlak.24@gmail.com>
2024-11-23 21:25:35 +0100

    Added chart

---
+++ Frontend/src/components/CustomPieChart.tsx
@@ -0,0 +1,65 @@
+import * as React from "react"
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export default function CustomPieChart({ chartConfig, chartData }) {
+    const totalVisitors = React.useMemo(() => {
+        return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
+    }, [])
+
+    return (
+        <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[250px]"
+        >
+            <PieChart>
+                <ChartTooltip
+                    cursor={false}
+                    content={<ChartTooltipContent hideLabel />}
+                />
+                <Pie
+                    data={chartData}
+                    dataKey="visitors"
+                    nameKey="browser"
+                    innerRadius={60}
+                    strokeWidth={5}
+                >
+                    <Label
+                        content={({ viewBox }) => {
+                            if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                return (
+                                    <text
+                                        x={viewBox.cx}
+                                        y={viewBox.cy}
+                                        textAnchor="middle"
+                                        dominantBaseline="middle"
+                                    >
+                                        <tspan
+                                            x={viewBox.cx}
+                                            y={viewBox.cy}
+                                            className="fill-foreground text-3xl font-bold"
+                                        >
+                                            {totalVisitors.toLocaleString()}
+                                        </tspan>
+                                        <tspan
+                                            x={viewBox.cx}
+                                            y={(viewBox.cy || 0) + 24}
+                                            className="fill-muted-foreground"
+                                        >
+                                            Visitors
+                                        </tspan>
+                                    </text>
+                                )
+                            }
+                        }}
+                    />
+                </Pie>
+            </PieChart>
+        </ChartContainer>
+    )
+}
