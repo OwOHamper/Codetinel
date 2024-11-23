d8309a12a9056f7cb6ec2573bfc2a85165812dfb
j4ku6-7 <j34543334@gmail.com>
2024-11-24 00:03:46 +0100

    added vuln_gets

--- Backend/src/api/router/main.py
+++ Backend/src/api/router/main.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from src.api.routes import project
 from src.api.routes import agent
+from src.api.routes import vulnerabilities
 
 api_router = APIRouter()
 
 api_router.include_router(project.router, prefix="/project", tags=["project"])
 api_router.include_router(agent.router, prefix="/agent", tags=["agent"])
+api_router.include_router(vulnerabilities.router, prefix="/vulnerabilities", tags=["vulnerabilities"])
---
+++ Backend/src/api/routes/vulnerabilities.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, HTTPException
+from bson import ObjectId
+from src.db.mongodb import get_database
+from src.utils.get_vulnerabilities import get_vulnerabilities_by_id, get_vulnerability_by_id
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/get-vulnerabilities/{project_id}")
+async def get_vulnerabilities_by_project(project_id: str):
+    return await get_vulnerabilities_by_id(project_id) 
+
+@router.get("/get-vulnerability/{project_id}/{vulnerability_id}")
+async def get_vulnerability_by_project_vulnerability(project_id: str, vulnerability_id: str):
+    return await get_vulnerability_by_id(project_id, vulnerability_id)
