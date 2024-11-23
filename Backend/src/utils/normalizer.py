994b0f4c365a865bb03cb788987f5b35379caaa9
j4ku6-7 <j34543334@gmail.com>
2024-11-23 20:50:54 +0100

    updated pentest agent, project, normalizer

--- Backend/src/utils/normalizer.py
+++ Backend/src/utils/normalizer.py
@@ -1,7 +1,11 @@
 import uuid
 def normalize_csv(csv_data):
-        return {
-        str(uuid.uuid4()): {key.strip().replace(' ', '_'): value for key, value in item.items()} for item in csv_data 
+        output = {
+        str(uuid.uuid4()): {key.strip().replace(' ', '_'): value for key, value in item.items()} for item in csv_data
         }
+        for i in output:
+            output[i]["Status"] = "not_started"
+        
+        return output
     
     
