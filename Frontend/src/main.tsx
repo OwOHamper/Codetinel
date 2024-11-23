5901525fd37caa78dd5a5c8d72c8b9d6753bf478
tomas-sedlak <t.sedlak.24@gmail.com>
2024-11-24 00:53:21 +0100

    Added react query to project page

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -6,6 +6,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // Import pages
 import Projects from '@/pages/Projects';
@@ -13,15 +14,20 @@ import CreateProject from '@/pages/CreateProject';
 import Project from '@/pages/Project';
 import Detail from './pages/Detail';
 
+// Create a client instance
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/new" element={<CreateProject />} />
-        <Route path="/projects/:projectId" element={<Project />} />
-        <Route path="/projects/:projectId/error/:errorId" element={<Detail />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/new" element={<CreateProject />} />
+          <Route path="/projects/:projectId" element={<Project />} />
+          <Route path="/projects/:projectId/error/:errorId" element={<Detail />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   </StrictMode>,
 )
