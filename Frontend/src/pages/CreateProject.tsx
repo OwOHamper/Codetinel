19c981fb6c20245f04796dd8450f914b5ff2a6ce
tomas-sedlak <t.sedlak.24@gmail.com>
2024-11-24 09:42:28 +0100

    Added back back arrows

--- Frontend/src/pages/CreateProject.tsx
+++ Frontend/src/pages/CreateProject.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { Pencil, Link, CloudUpload } from "lucide-react"
+import { Pencil, Link, CloudUpload, ArrowLeft } from "lucide-react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 
@@ -47,6 +47,15 @@ export default function CreateProject() {
   return (
     <form onSubmit={handleSubmit} className="max-w-screen-lg mx-auto p-4">
       <div className="mt-20">
+        <div className="mb-4 flex items-center">
+          <button
+            onClick={() => navigate(-1)}
+            className="flex items-center gap-2 text-gray-600 hover:text-gray-900 transition-colors"
+          >
+            <ArrowLeft className="w-5 h-5" />
+            <span>Back</span>
+          </button>
+        </div>
         <h1 className="text-4xl font-black tracking-tight lg:text-5xl text-indigo-600">Title</h1>
         <div className="relative">
           <Input
@@ -70,31 +79,31 @@ export default function CreateProject() {
       <div>
         <h1 className="text-4xl font-black tracking-tight lg:text-5xl mt-5 text-indigo-600">Insert data</h1>
 
-          <div className="relative">
-            <Input
-              type="text"
-              placeholder="Write the url of your source code"
-              value={deployUrl}
-              onChange={(e) => setDeployUrl(e.target.value)}
-              className="mt-4 hover:bg-gray-100 pl-9"
-            />
-            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
-              <Link className="h-4 w-4" />
-            </span>
-          </div>
+        <div className="relative">
+          <Input
+            type="text"
+            placeholder="Write the url of your source code"
+            value={deployUrl}
+            onChange={(e) => setDeployUrl(e.target.value)}
+            className="mt-4 hover:bg-gray-100 pl-9"
+          />
+          <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
+            <Link className="h-4 w-4" />
+          </span>
+        </div>
 
-          <div className="relative">
-            <Input
-              type="text"
-              placeholder="Write the url of the deployment which should be tested on"
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-              className="mt-2 hover:bg-gray-100 pl-9"
-            />
-            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
-              <Link className="h-4 w-4" />
-            </span>
-          </div>
+        <div className="relative">
+          <Input
+            type="text"
+            placeholder="Write the url of the deployment which should be tested on"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            className="mt-2 hover:bg-gray-100 pl-9"
+          />
+          <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
+            <Link className="h-4 w-4" />
+          </span>
+        </div>
 
         <div className="relative">
           <Input
