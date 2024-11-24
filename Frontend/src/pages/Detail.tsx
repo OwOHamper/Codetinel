5bb0b5934fef925834a7de3d8ed24f2a62dbb743
mark-takac <steam.omg.mk@gmail.com>
2024-11-24 07:49:00 +0100

    Added bold text

--- Frontend/src/pages/Detail.tsx
+++ Frontend/src/pages/Detail.tsx
@@ -32,7 +32,7 @@ export default function Detail() {
     )
   }
 
-  {console.log(vulnerability?.details.replace("\n$", ''))}
+  { console.log(vulnerability?.details.replace("\n$", '')) }
 
   return (
     <main className="max-w-screen-lg mx-auto p-4">
@@ -46,37 +46,41 @@ export default function Detail() {
         <p className="font-bold text-2xl text-indigo-600">Summary of the Vulnerability</p>
       </div>
       <p className="mb-4">{vulnerability?.details.split("\\n")}</p>
-      
 
-      {vulnerability?.last_test && (
-        <>
-          <div className="mb-4">
-            <p className="font-bold text-2xl text-indigo-600 mb-4">Suggestion</p>
-            <p>{vulnerability.last_test.result.suggestion}</p>
-          </div>
-          <div>
-            <p className="font-bold text-2xl text-indigo-600 mb-4">Code overview</p>
-            <p className="mb-2">Vulnerabilty found on line {vulnerability.last_test.result.line_number}</p>
-            <div className="border border-black rounded-lg p-4 bg-gray-50">
-              <pre className="whitespace-pre-wrap font-mono text-sm">
-                {vulnerability.last_test.result.file_context.split("\n").map((line, idx) => {
-                  const lineNumber = vulnerability.last_test.result.file_context.split("\n")[idx].split("|")[0].trim(); // Extract the line number
-                  return (
-                    <div
-                      key={lineNumber}
-                      className={parseInt(lineNumber) === vulnerability.last_test.result.line_number ? "bg-red-200" : ""}
-                    >
-                      {line}
-                    </div>
-                  );
-                })}
-              </pre>
+
+      {vulnerability?.last_test ? (
+        vulnerability?.last_test.result ? (
+          <>
+            <div className="mb-4">
+              <p className="font-bold text-2xl text-indigo-600 mb-4">Suggestion</p>
+              <p>{vulnerability.last_test.result.suggestion}</p>
             </div>
-          </div>
-        </>
-      ) || (
-          <p>Vulnerability not tested yet</p>
-        )}
+            <div>
+              <p className="font-bold text-2xl text-indigo-600 mb-4">Code overview</p>
+              <p className="mb-2">Vulnerabilty found on <b>line {vulnerability.last_test.result.line_number}</b></p>
+              <div className="border border-black rounded-lg p-4 bg-gray-50">
+                <pre className="whitespace-pre-wrap font-mono text-sm">
+                  {vulnerability.last_test.result.file_context.split("\n").map((line, idx) => {
+                    const lineNumber = vulnerability.last_test.result.file_context.split("\n")[idx].split("|")[0].trim(); // Extract the line number
+                    return (
+                      <div
+                        key={lineNumber}
+                        className={parseInt(lineNumber) === vulnerability.last_test.result.line_number ? "bg-red-200" : ""}
+                      >
+                        {line}
+                      </div>
+                    );
+                  })}
+                </pre>
+              </div>
+            </div>
+          </>
+        ) : (
+          <p>The AI agent couldn't exploit the vulnerabilty on your site</p>
+        )
+      ) : (
+        <p>Vulnerability not tested yet</p>
+      )}
       {/* <div className="border border-black rounded-lg p-4"> */}
 
       {/* </div> */}
